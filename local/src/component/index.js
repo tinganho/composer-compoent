b9adea258bd1f1d3560c8fd4030f816bab494e1d
Tingan Ho <tingan87@gmail.com>
2015-07-30 18:50:31 +0800

    Adds typescript compiler install and removes local folder

--- local/src/component/index.js
+++
@@ -1,4 +0,0 @@
-var createElement_1 = require('./createElement');
-exports.createElement = createElement_1.createElement;
-var component_1 = require('./component');
-exports.Component = component_1.Component;
