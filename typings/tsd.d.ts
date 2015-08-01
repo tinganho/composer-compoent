31eca0e5ec1969e9852e6269a5d7ef3ee59e27ac
Tingan Ho <tingan87@gmail.com>
2015-08-01 11:44:12 +0800

    Removes open.d.ts

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -5,7 +5,6 @@
 /// <reference path="browserify/browserify.d.ts" />
 /// <reference path="chai/chai.d.ts" />
 /// <reference path="mocha/mocha.d.ts" />
-/// <reference path="open/open.d.ts" />
 /// <reference path="express/express.d.ts" />
 /// <reference path="serve-static/serve-static.d.ts" />
 /// <reference path="mime/mime.d.ts" />
